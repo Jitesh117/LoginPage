4502a67aac1b89532b662cfb8aa20d12070d0ac1
Jitesh117 <sahookumarjitesh@gmail.com>
2022-08-01 19:48:09 +0530

    initial commit

---
+++ lib/login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade200,
+    );
+  }
+}
